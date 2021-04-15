8556391ce70cff4b7f5c5733234c9d7ed76e71a9
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-04-15 22:21:37 +0600

    Windows 8 calculator

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.buttonSeven = new System.Windows.Forms.Button();
+            this.buttonEight = new System.Windows.Forms.Button();
+            this.buttonNine = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.buttonMultiplication = new System.Windows.Forms.Button();
+            this.buttonSix = new System.Windows.Forms.Button();
+            this.buttonFive = new System.Windows.Forms.Button();
+            this.buttonFour = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonThree = new System.Windows.Forms.Button();
+            this.buttonTwo = new System.Windows.Forms.Button();
+            this.buttonOne = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonZero = new System.Windows.Forms.Button();
+            this.buttonDot = new System.Windows.Forms.Button();
+            this.buttonCE = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonEqual = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSeven
+            // 
+            resources.ApplyResources(this.buttonSeven, "buttonSeven");
+            this.buttonSeven.Name = "buttonSeven";
+            this.buttonSeven.UseVisualStyleBackColor = true;
+            this.buttonSeven.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonEight
+            // 
+            resources.ApplyResources(this.buttonEight, "buttonEight");
+            this.buttonEight.Name = "buttonEight";
+            this.buttonEight.UseVisualStyleBackColor = true;
+            this.buttonEight.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonNine
+            // 
+            resources.ApplyResources(this.buttonNine, "buttonNine");
+            this.buttonNine.Name = "buttonNine";
+            this.buttonNine.UseVisualStyleBackColor = true;
+            this.buttonNine.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonDivision
+            // 
+            resources.ApplyResources(this.buttonDivision, "buttonDivision");
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.UseVisualStyleBackColor = true;
+            this.buttonDivision.Click += new System.EventHandler(this.Operation_Click);
+            // 
+            // buttonMultiplication
+            // 
+            resources.ApplyResources(this.buttonMultiplication, "buttonMultiplication");
+            this.buttonMultiplication.Name = "buttonMultiplication";
+            this.buttonMultiplication.UseVisualStyleBackColor = true;
+            this.buttonMultiplication.Click += new System.EventHandler(this.Operation_Click);
+            // 
+            // buttonSix
+            // 
+            resources.ApplyResources(this.buttonSix, "buttonSix");
+            this.buttonSix.Name = "buttonSix";
+            this.buttonSix.UseVisualStyleBackColor = true;
+            this.buttonSix.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonFive
+            // 
+            resources.ApplyResources(this.buttonFive, "buttonFive");
+            this.buttonFive.Name = "buttonFive";
+            this.buttonFive.UseVisualStyleBackColor = true;
+            this.buttonFive.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonFour
+            // 
+            resources.ApplyResources(this.buttonFour, "buttonFour");
+            this.buttonFour.Name = "buttonFour";
+            this.buttonFour.UseVisualStyleBackColor = true;
+            this.buttonFour.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonMinus
+            // 
+            resources.ApplyResources(this.buttonMinus, "buttonMinus");
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.Operation_Click);
+            // 
+            // buttonThree
+            // 
+            resources.ApplyResources(this.buttonThree, "buttonThree");
+            this.buttonThree.Name = "buttonThree";
+            this.buttonThree.UseVisualStyleBackColor = true;
+            this.buttonThree.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonTwo
+            // 
+            resources.ApplyResources(this.buttonTwo, "buttonTwo");
+            this.buttonTwo.Name = "buttonTwo";
+            this.buttonTwo.UseVisualStyleBackColor = true;
+            this.buttonTwo.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonOne
+            // 
+            resources.ApplyResources(this.buttonOne, "buttonOne");
+            this.buttonOne.Name = "buttonOne";
+            this.buttonOne.UseVisualStyleBackColor = true;
+            this.buttonOne.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonPlus
+            // 
+            resources.ApplyResources(this.buttonPlus, "buttonPlus");
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.Operation_Click);
+            // 
+            // buttonZero
+            // 
+            resources.ApplyResources(this.buttonZero, "buttonZero");
+            this.buttonZero.Name = "buttonZero";
+            this.buttonZero.UseVisualStyleBackColor = true;
+            this.buttonZero.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonDot
+            // 
+            resources.ApplyResources(this.buttonDot, "buttonDot");
+            this.buttonDot.Name = "buttonDot";
+            this.buttonDot.UseVisualStyleBackColor = true;
+            this.buttonDot.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonCE
+            // 
+            resources.ApplyResources(this.buttonCE, "buttonCE");
+            this.buttonCE.Name = "buttonCE";
+            this.buttonCE.UseVisualStyleBackColor = true;
+            this.buttonCE.Click += new System.EventHandler(this.buttonCE_Click);
+            // 
+            // buttonC
+            // 
+            resources.ApplyResources(this.buttonC, "buttonC");
+            this.buttonC.Name = "buttonC";
+            this.buttonC.UseVisualStyleBackColor = true;
+            this.buttonC.Click += new System.EventHandler(this.buttonC_Click);
+            // 
+            // buttonEqual
+            // 
+            resources.ApplyResources(this.buttonEqual, "buttonEqual");
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.UseVisualStyleBackColor = true;
+            this.buttonEqual.Click += new System.EventHandler(this.buttonEqual_Click);
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Calculator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonEqual);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonCE);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonZero);
+            this.Controls.Add(this.buttonDot);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonThree);
+            this.Controls.Add(this.buttonTwo);
+            this.Controls.Add(this.buttonOne);
+            this.Controls.Add(this.buttonMultiplication);
+            this.Controls.Add(this.buttonSix);
+            this.Controls.Add(this.buttonFive);
+            this.Controls.Add(this.buttonFour);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.buttonNine);
+            this.Controls.Add(this.buttonEight);
+            this.Controls.Add(this.buttonSeven);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSeven;
+        private System.Windows.Forms.Button buttonEight;
+        private System.Windows.Forms.Button buttonNine;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.Button buttonMultiplication;
+        private System.Windows.Forms.Button buttonSix;
+        private System.Windows.Forms.Button buttonFive;
+        private System.Windows.Forms.Button buttonFour;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonThree;
+        private System.Windows.Forms.Button buttonTwo;
+        private System.Windows.Forms.Button buttonOne;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonZero;
+        private System.Windows.Forms.Button buttonDot;
+        private System.Windows.Forms.Button buttonCE;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonEqual;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+       
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        Double result = 0;
+        String operation = "";
+        bool operationCheck = false;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if((richTextBox1.Text == "0") || (operationCheck))
+            {
+                richTextBox1.Clear();
+            }
+
+            operationCheck = false;
+            Button button = (Button)sender;
+
+            if (button.Text==".")
+            {
+                if(!richTextBox1.Text.Contains("."))
+                    {
+                    richTextBox1.Text = richTextBox1.Text + button.Text;
+                }
+
+            }else
+            richTextBox1.Text = richTextBox1.Text + button.Text;
+            
+        }
+
+        private void Operation_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = Double.Parse(richTextBox1.Text);
+            label2.Text = result + " " + operation;
+            operationCheck = true;
+        }
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "0";
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "0";
+            result = 0;
+
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            switch(operation)
+            {
+                case "+":
+                    richTextBox1.Text = (result + Double.Parse(richTextBox1.Text)).ToString();
+                    break;
+                case "-":
+                    richTextBox1.Text = (result - Double.Parse(richTextBox1.Text)).ToString();
+                    break;
+                case "X":
+                    richTextBox1.Text = (result * Double.Parse(richTextBox1.Text)).ToString();
+                    break;
+                case "/":
+                    richTextBox1.Text = (result / Double.Parse(richTextBox1.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
